63d855a8698759ff801ec26c7de2aa3b80f34a30
Vikas Ahuja <test.wishnbuy@gmail.com>
2016-11-16 16:16:06 +0530

    Add files via upload

---
+++ Basic_Program1.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Basic_Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//System.out.println("Hello");
+		System.out.println("Hello\nAlexandra Abramov!");
+	}
+
+}
